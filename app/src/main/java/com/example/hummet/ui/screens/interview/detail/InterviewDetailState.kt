68a2adddf7c8a093a1ed51f2b20812b05b494685
Hümmət Əzimzadə <69711064+hummxt@users.noreply.github.com>
2026-02-17 06:07:23 +1100

    chore: replace files

--- app/src/main/java/com/example/hummet/ui/screens/interview/detail/InterviewDetailState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.hummet.ui.screens.interview.detail
-
-sealed class InterviewDetailState {
-    object Loading : InterviewDetailState()
-    data class Success(val detail: com.example.hummet.data.model.TopicDetail) : InterviewDetailState()
-    data class Error(val message: String) : InterviewDetailState()
-}
